d76f685e146c752e86fb43ec0005d7411e4348f9
Artem Bolshakov <a.t.bolsh@gmail.com>
2021-01-12 05:27:22 -0800

    Further plans added.

--- shortcutAgents.py
+++ shortcutAgents.py
@@ -8,10 +8,27 @@
 # the limited predicates, Might also avoid the "exponential growth of functions" 
 # problem that keeps happening.
 
-# This is really the crux of the 'imagination' portion; with continuous 2d agents,
-# this will involve teleporting accross the map and imagining that the agent is absent,
+# This is really the crux of the 'simulation' portion; with continuous 2d agents,
+# this will involve teleporting accross the map and simulating that the agent is absent,
 # etc.
 
 from agent import *
 from simulator import *
 from perceptor import *
+
+# For apples, the some actual methods needed:
+
+# 1) Take(X) (from anywhere)
+# 2) Remove(X) (disappears; goes to a 'limbo' dict.) Limbo will be useful.
+# 3) Fuse(X, Y): If On(Y, X), then these two will become a single object.
+# 4) Unfuse(Y): splits it into just two parts.
+# 5) Move(X, From, To): X teleports
+
+# All of this will correspond to actions defined in a new STRIPS file.
+# There also need to be simple ways to turn off one or more of these actions.
+# This can be added in the STRIPS file (eg, one of the consequences is RemoveActionUsed, and you can't undo that predicate).
+
+# I really need to start thinking about how to define subproblems and solve them in a submethod, 
+# OR 
+# Make limiting the specific set of literals under consideration part of executing a plan.
+
